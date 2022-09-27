7c626595a7a566c7b88788fbf7d7724336d1617e
levanufriev02 <levanufriev02@gmail.com>
2022-09-27 20:29:41 +0300

    Add InMemoryRepository

---
+++ Catalog/Repositories/InMemoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.Models;
+using System;
+
+namespace Catalog.Repositories
+{
+    public class InMemoryRepository
+    {
+        private readonly List<Item> items = new()
+        {
+            new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreationDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreationDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 18, CreationDate = DateTimeOffset.UtcNow }
+        };
+    }
+}
