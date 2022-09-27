b11d0e8b725903b1ea0ab81d0f9355d539fd47f2
levanufriev02 <levanufriev02@gmail.com>
2022-09-27 20:56:10 +0300

    Add Controller. Add Get And GetById methods.

---
+++ Catalog/Controllers/ItemsController.cs
@@ -0,0 +1,36 @@
+﻿using Catalog.Models;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly InMemoryRepository repository;
+
+        public ItemsController()
+        {
+            repository = new InMemoryRepository();
+        }
+
+        [HttpGet]
+        public IEnumerable<Item> GetItems() 
+            => repository.GetItems();
+
+        [HttpGet("id")]
+        public ActionResult<Item> GetItem(Guid id)
+        {
+            var item = repository.GetItem(id);
+
+            if (item is null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
+
+    }
+}
--- Catalog/Repositories/InMemoryRepository.cs
+++ Catalog/Repositories/InMemoryRepository.cs
@@ -11,5 +11,11 @@ namespace Catalog.Repositories
             new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreationDate = DateTimeOffset.UtcNow },
             new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 18, CreationDate = DateTimeOffset.UtcNow }
         };
+
+        public IEnumerable<Item> GetItems()
+            => items;
+
+        public Item GetItem(Guid id)
+            => items.Where(item => item.Id == id).FirstOrDefault();
     }
 }
